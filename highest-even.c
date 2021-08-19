285bb39814a492f01b382ad48fc2e8313d935f55
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-08-20 03:51:10 +0800

    Create highest-even.c

---
+++ highest-even.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int i = 0;
+    int max_even = 0;
+    while(n != 0){
+	    scanf("%d", &n);
+        if(n % 2 == 0 && max_even < n){
+            max_even = n;
+            i++;
+	    }
+    }
+    if(max_even > 0){
+        printf("The highest EVEN integer is %d", max_even);
+    } else {
+        printf("No EVEN integer found!");
+    }
+    return 0;
+}
